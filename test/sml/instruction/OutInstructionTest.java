75ec066b96155e21634d6587312f8103bafbb4a7
Chaitali Desale <cdesal01@student.bbk.ac.uk>
2023-03-02 23:33:58 +0000

    Add test for OutInstruction.

---
+++ test/sml/instruction/OutInstructionTest.java
@@ -0,0 +1,45 @@
+package sml.instruction;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import sml.Instruction;
+import sml.Machine;
+import sml.Registers;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.PrintStream;
+
+import static sml.Registers.Register.EAX;
+
+public class OutInstructionTest {
+    private Machine machine;
+    private Registers registers;
+    private ByteArrayOutputStream outContent;
+
+    @BeforeEach
+    void setUp() {
+        machine = new Machine(new Registers());
+        registers = machine.getRegisters();
+        outContent = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(outContent));
+    }
+
+    @AfterEach
+    void tearDown() throws IOException {
+        machine = null;
+        registers = null;
+        System.setOut(System.out);
+        outContent.close();
+    }
+
+    @Test
+    void executeValid() {
+        registers.set(EAX, 5);
+        Instruction instruction = new OutInstruction(null, EAX);
+        instruction.execute(machine);
+        Assertions.assertEquals("5" + System.lineSeparator(), outContent.toString());
+    }
+}
